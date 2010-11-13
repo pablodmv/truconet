2b8882f2432dfa916ce9b674e3c51dca1c415737
leitesgustavo <leitesgustavo@13e10736-ecc6-9a4e-9c23-c025aa741aa0>
2010-11-13 00:51:22 +0000

    Carta, Credencial, Partido, Ranking, Usuario

--- trunk/truconet/truconet/Carta.cs
+++ trunk/truconet/truconet/Carta.cs
@@ -7,5 +7,43 @@ namespace truconet
 {
     class Carta
     {
+        private int cod;
+        public enum palo {Oro, Copa, Basto, Espada};
+        public enum categoria {Pieza, Mata, Fio, Comun};
+        private int numero;
+        private int puntaje;
+
+        //Constructores
+        public Carta()
+        {
+            
+        }
+
+
+        //Getters y Setters
+        public int Cod
+        {
+            get{ return cod; }
+            set{ cod = value; }
+        }
+
+
+        public int Numero
+        {
+            get{ return numero; }
+            set{ numero = value; }
+        }
+
+
+        public int Puntaje
+        {
+            get{ return puntaje; }
+            set{ puntaje = value; }
+        }
+
+
+
+
+
     }
 }
--- trunk/truconet/truconet/Ranking.cs
+++ trunk/truconet/truconet/Ranking.cs
@@ -7,5 +7,43 @@ namespace truconet
 {
     class Ranking
     {
+
+        private int posicion;
+        private Jugador jug;
+        private String puntaje;
+
+
+        //Constructores
+
+        public Ranking()
+        {
+        }
+
+
+        //Getters y Setters
+
+        public String Puntaje
+        {
+            get { return puntaje; }
+            set { puntaje = value; }
+        }
+
+
+        public Jugador Jug
+        {
+            get { return jug; }
+            set { jug = value; }
+        }
+        
+
+
+        public int Posicion
+        {
+            get { return posicion; }
+            set { posicion = value; }
+        }
+
+        
+
     }
 }
