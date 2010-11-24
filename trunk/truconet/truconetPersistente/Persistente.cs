26f3510fdd8af581aa891a00024cfc1a26c37253
leitesgustavo <leitesgustavo@13e10736-ecc6-9a4e-9c23-c025aa741aa0>
2010-11-24 01:37:32 +0000

    trucoPersistente

---
+++ trunk/truconet/truconetPersistente/Persistente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+
+namespace truconetPersistente
+{
+    public class Persistente
+    {
+        protected String conn = "integrated security=SSPI;data source=GUSTAVOLEITES1;initial catalog=truconetDB";
+
+        public Persistente()
+		{
+			
+		}
+
+    }
+}
